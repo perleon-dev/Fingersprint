1b941324bf6a77e80e68fe2fc7de3c173aaad079
hangeKennedy <victorleon4520@gmail.com>
2024-04-02 11:25:03 -0500

    Se agrego diseño a la pagina web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,27 @@ import { User } from './user';
 })
 export class AppComponent implements OnInit{
   title = 'fingersprint_Virdi';
+  user?: User;
+  imageBase64?:string;
+
+  public messages: User[] = [];
 
   constructor(private webSocketService: WebSocketService) { }
 
   ngOnInit(): void {
     this.webSocketService.connect().subscribe(
       (message) => {
-        console.log("Conecto : "+ message);
+        this.user = JSON.parse(message);
+        
+        if(this.user != undefined && this.user.msgId == 2001){
+          var xx = JSON.parse(this.user?.body)
+          this.imageBase64 = xx.imageData;
+          console.log("Conecto : "+ JSON.stringify(this.user) );
+        }else{
+          console.log("Conecto : "+ message );
+        }
+
+        
       },
       (error) => {
         console.error(error);
@@ -23,7 +37,8 @@ export class AppComponent implements OnInit{
     );
   }
 
-  sendMessage(): void {
-    this.webSocketService.sendMessage('{"msgId":"2000","body":{"UserId":"","ImageType":"JPG","BrandType":"VIRDI","FingerIndex":1,"TemplateFormat":3}}');
+  //fingerindes numero de dedo
+  sendMessage(numDedo: string): void {
+    this.webSocketService.sendMessage('{"msgId":"2000","body":{"UserId":"","ImageType":"JPG","BrandType":"VIRDI","FingerIndex":'+numDedo+',"TemplateFormat":3}}');
   }
 }
