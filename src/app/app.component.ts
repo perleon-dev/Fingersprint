1306bf68d386c93465c4cf588964ee0be7237dda
hangeKennedy <victorleon4520@gmail.com>
2024-04-04 13:08:57 -0500

    Se agrego un input con la trama

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WebSocketService } from './service/web-socket.service';
 import { User } from './user';
+import { FingerTrama } from './finger-trama';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,13 @@ import { User } from './user';
 
 export class AppComponent implements OnInit{
   title = 'fingersprint_Virdi';
+  //ImageData
   user?: User;
   imageBase64?:string;
+
+  //Fingedata
+  fingerTrama?: FingerTrama; 
+  data?:string;
 
   public messages: User[] = [];
 
@@ -23,14 +29,26 @@ export class AppComponent implements OnInit{
     this.webSocketService.connect().subscribe(
       (message) => {
         this.user = JSON.parse(message);
+        this.fingerTrama = this.user;
         
         if(this.user != undefined && this.user.msgId == 2001){
           var xx = JSON.parse(this.user?.body)
+
           this.imageBase64 = xx.imageData;
+
+
           console.log("Conecto : "+ JSON.stringify(this.user) );
 
-        }else{
-          console.log("Conecto : "+ message );
+
+        } else if(this.fingerTrama != undefined && this.fingerTrama.msgId == 2002){
+          var yy = JSON.parse(this.fingerTrama?.body)
+
+          this.data = yy.FingerData;
+
+          console.log("Conecto : "+ JSON.stringify(this.fingerTrama) );
+        }
+        else{
+          //console.log("Conecto : "+ message );
         }
 
         
---
+++ src/app/finger-trama.ts
@@ -0,0 +1,4 @@
+export interface FingerTrama {
+    msgId: number;
+    body: any;
+}
